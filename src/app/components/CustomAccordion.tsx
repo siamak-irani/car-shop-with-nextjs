192019341bbefc5d65c23e0c49e9dd28f537d481
Siamak Irani <siamakirani1378@gmail.com>
2024-03-24 20:43:25 +0330

    CustomAccordion added

---
+++ src/app/components/CustomAccordion.tsx
@@ -0,0 +1,86 @@
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import Link from "next/link";
+import React from "react";
+
+type CustomAccordionProps = {
+  items: NavLink[];
+  onClick: (event: React.KeyboardEvent | React.MouseEvent) => void;
+  onKeyDown: (event: React.KeyboardEvent | React.MouseEvent) => void;
+};
+
+const CustomAccordion = (props: CustomAccordionProps) => {
+  return (
+    <>
+      <Accordion
+        disableGutters
+        sx={{
+          boxShadow: "none",
+          width: "100%",
+          background: "transparent",
+          borderRadius: "0",
+        }}
+      >
+        <AccordionSummary
+          expandIcon={
+            <ArrowDropDownIcon
+              sx={{
+                fill: "green",
+              }}
+            />
+          }
+          aria-controls="panel2-content"
+          id="panel2-header"
+          sx={{
+            display: "grid",
+            gridTemplateColumns: "1fr auto 1fr",
+            justifyContent: "center",
+            "& .MuiAccordionSummary-content": {
+              fontSize: "12px",
+              flexGrow: "0",
+              gridColumn: "2",
+              "& .MuiListItemText-root": {
+                fontSize: "12px",
+              },
+            },
+            "& .MuiAccordionSummary-expandIconWrapper": {
+              width: "fit-content",
+            },
+          }}
+        >
+          <Typography>
+            <ListItemText
+              disableTypography
+              primary={"test"}
+              sx={{
+                fontSize: "14px !important",
+                color: "black",
+                textAlign: "center",
+              }}
+            />
+          </Typography>
+        </AccordionSummary>
+        {props.items.map((i) => {
+          return (
+            <AccordionDetails
+              onClick={props.onClick}
+              onKeyDown={props.onKeyDown}
+            >
+              <Typography sx={{ textAlign: "center", fontSize: "14px" }}>
+                <Link href={i.url}>{i.text}</Link>
+              </Typography>
+            </AccordionDetails>
+          );
+        })}
+      </Accordion>
+    </>
+  );
+};
+
+export default CustomAccordion;
