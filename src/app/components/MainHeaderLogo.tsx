51df87f00dfeb369f1c83a6484353cd1b28551e0
Siamak Irani <siamakirani1378@gmail.com>
2024-03-24 20:51:28 +0330

    MainHeaderLogo added

---
+++ src/app/components/MainHeaderLogo.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import React from "react";
+
+const MainHeaderLogo = () => {
+  return (
+    <>
+      <Image src="/Logo Area.png" alt="logo" width={40} height={40} />
+    </>
+  );
+};
+
+export default MainHeaderLogo;
